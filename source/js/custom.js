410a724f6d9d4bbb6fa0756b9ff6b85af3fb5359
Yuval Cohen <yuval@cohenyuval.com>
2022-08-07 14:51:46 +0300

    making website add baseline mrqen website

---
+++ source/js/custom.js
@@ -0,0 +1,25 @@
+function random_range(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomize_position(i, obj) {
+
+	$(obj).css('margin-top', random_range(20, 50) + "px");
+	tilt_degs = "rotate(" + random_range(-40, 41) + "deg)";
+	$(obj).css('transform', tilt_degs);
+	$(obj).css('-webkit-transform', tilt_degs);
+	$(obj).css('-moz-transform', tilt_degs);
+	$(obj).css('-o-transform', tilt_degs);
+	$(obj).show();
+}
+
+$(window).load(function()
+{
+    $.each($(".tool_logo"), randomize_position);
+
+    setTimeout(function () {
+    	if ($(window).scrollTop() == 0) {
+			$("#services h1").animate({"margin-top": "-=150px", "margin-bottom": "+=150px"}, 200, 'easeOutCubic').animate({"margin-top": "+=150px", "margin-bottom": "-=150px"}, 200, 'easeInCubic');
+    	}
+    }, 5000);
+});
